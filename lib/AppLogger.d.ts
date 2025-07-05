c584ddf768ba515488bf555fa36c8d051b7c4665
Aman Shekhar <aman.shekhar4@gmail.com>
2025-07-05 22:08:43 +0530

    Clean up tracked build and dependency folders

--- lib/AppLogger.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class AppLogger {
-    static e(tag: string, msg: any): void;
-    static i(tag: string, msg: any): void;
-    static w(tag: string, msg: any): void;
-    static d(tag: string, msg: any): void;
-}
